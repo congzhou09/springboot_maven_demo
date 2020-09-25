93ac10e4bcecc18229a3b0e739f53749060e0501
congzhou09 <yuancongzhou@yeah.net>
2020-09-25 11:09:50 +0800

    初始化

---
+++ src/main/java/com/congzhou/SpringbootMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.congzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMavenDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/congzhou/SpringbootMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congzhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMavenDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
